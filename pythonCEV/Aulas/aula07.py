b3ed60592ef280c00a7f2a5d843c50d05d22e636
Paulo Henrique Ito Gabriel <40235350+PauloHIG@users.noreply.github.com>
2020-03-28 19:49:36 -0300

    Add files via upload

--- pythonCEV/Aulas/aula07.py
+++ pythonCEV/Aulas/aula07.py
@@ -1,5 +1,4 @@
 #Operadores aritimeticos
-
 #funcoes de operaçoes
 def soma(n1,n2):
     return n1 + n2
@@ -16,38 +15,15 @@ def modu(n1,n2):
 def pot(n1,n2):
     return n1 ** n2 #potenciaçao
 
+#potenciação ²,³ e a de qualquer numero respectivamente
 def raizq(n1):
     return n1**(1/2)
 def raizcub(n1):
     return n1**(1/3)
 def raiz(n1,r):
     return n1**(1/r)
-#tabuada
+#tabuada do 1 ao 10 de qualquer numero
 def tabuada(n):
     print("\ntabuada do {} :".format(n))
     for i in range(1, 11):
         print('{} X {} = {}'.format(n, i, n * i))
-
-
-
-'''
-print(raizq(81))
-print(raizcub(27))
-ordem de precedencia em python
-1º = ()
-2º = **
-3º = * / % //
-4º = + -
-
-n1 = int(input('Digite um numero: '))
-n2 = int(input('Digite outro numero: '))
-s = n1+n2
-d = n1/n2
-di= n1//n2
-m = n1*n2
-p = n1**n2
-r = n1 % n2
-
-print('A soma e {}, a multiplicaçao e {}, a divisao e {:.3f}, a divisao inteira e {}, o resto e {}, a potencia e {}'.format(s,m,d,di,r,p))
-
-o {:.3f} que se ve ali em cima e a limitaçao das casas decimais do numero flutuante'''
