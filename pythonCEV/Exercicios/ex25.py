b3bfbb13d7fd1ec8edde74a2ecdcabccacbd3507
Paulo Henrique Ito Gabriel <40235350+PauloHIG@users.noreply.github.com>
2020-05-03 01:34:22 -0300

    Add files via upload

---
+++ pythonCEV/Exercicios/ex25.py
@@ -0,0 +1,5 @@
+#025: Crie um programa que leia o nome de uma pessoa e diga se ela tem "SILVA" no nome.
+nome = input('Digite seu nome: ')
+print('Seu nome tem Silva?')
+print('silva' in nome.lower())
+#ficou parecido com a do curso 
---
+++ pythonCEV/Exercicios/ex27.py
@@ -0,0 +1,6 @@
+#027: Faça um programa que leia o nome completo de uma pessoa, mostrando em seguida o primeiro e o último nome separadamente.'''
+nome = str(input('Digite seu nome: '))
+n = nome.split()
+print('Nome:{}'.format(nome))
+print('Primeiro nome:{}'.format(n[0]))
+print('Ultimo nome: {}'.format(n[len(n)-1]))#gambiarra pra pegar o ultimo elemento da lista
