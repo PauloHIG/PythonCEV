e0f2dc0c8e8d245fdee675ea14ffa7545a7c627a
Paulo Henrique Ito Gabriel <40235350+PauloHIG@users.noreply.github.com>
2020-05-16 11:19:51 -0300

    Add files via upload

---
+++ pythonCEV/Aulas/aula10.py
@@ -0,0 +1,15 @@
+#aula 10
+#condições
+nome = input('Qual é seu nome?')
+if nome == 'Paulo':
+	print('Eu sei que você usou if no exercício 07')
+else:
+	print('Olá, {}'.format(nome))
+
+#ex 29 multa se a velocidade do carro for maior do que *80km por hora, 7R$ para cada quilometro excedente
+#ex 30 mostra se um número é par ou impar
+#ex 31 perguntar a distacia de uma viagem em KM, cobrando 0,50 centavos por km para viagens com menos de 200km e 0,45 para viagens mais longas
+#ex 32 faça um programa que mostra se o ano é bisexto, precisa pegar o ano atual sem que o usuario precise digitar
+#ex 33 ler três números e mostrar qual é o maior e  qual é o menor
+#ex 34 aumento, se o salario for >1250 o aumento é de 10%, para salários menores, o aumento é de 15%
+#ex 35 ler o comprimento de três retas e ver se elas podem formar um triângulo
