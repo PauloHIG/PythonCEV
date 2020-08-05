99ab465dae0421ec6fb82b16c77b9dbfa5b0fcf0
Paulo Henrique Ito Gabriel <40235350+PauloHIG@users.noreply.github.com>
2020-08-05 18:58:15 -0300

    Add files via upload

---
+++ pythonCEV/Exercicios/ex38.py
@@ -0,0 +1,8 @@
+n1=int(input('Digite o primeiro número: '))
+n2=int(input('Digite o segundo número: '))
+if n1 == n2:
+	print('{} é igual a {}'.format(n1,n2))
+elif n1>n2:
+	print('{} é maior que {}'.format(n1,n2))
+else:
+	print('{} é maior que {}'.format(n2,n1))
