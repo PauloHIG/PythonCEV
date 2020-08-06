752e5309f79dc42cf993eae8d9aa57d08fcf9096
Paulo Henrique Ito Gabriel <40235350+PauloHIG@users.noreply.github.com>
2020-08-05 21:58:30 -0300

    Add files via upload

---
+++ pythonCEV/Exercicios/ex39.py
@@ -0,0 +1,16 @@
+import datetime
+ano = datetime.date.today().year
+while True:
+	try:
+		idade = int(input('Em que ano você nasceu?: '))
+	except:
+		print('Somente o ano')
+		continue
+	break
+alistamento = idade+18
+if ano == alistamento:
+	print('Você tem que se alistar ainda esse ano')
+elif ano < alistamento:
+	print('O ano em que você deve se alistar é {}'.format(alistamento))
+elif ano > alistamento:
+	print('Você está atrasado para o alistamento, era pra ser feito em {}'.format(alistamento))
