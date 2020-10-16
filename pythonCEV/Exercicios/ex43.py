2ae03410e7e5c97ac42815dcdb90d2f82d4d8f02
Paulo Henrique Ito Gabriel <40235350+PauloHIG@users.noreply.github.com>
2020-10-16 20:11:10 -0300

    Add files via upload

---
+++ pythonCEV/Exercicios/ex43.py
@@ -0,0 +1,36 @@
+'''Exercício Python 043: Desenvolva uma lógica que leia o peso e a altura de uma pessoa, calcule seu Índice de Massa Corporal (IMC) e mostre seu status, de acordo com a tabela abaixo:
+- IMC abaixo de 18,5: Abaixo do Peso
+- Entre 18,5 e 25: Peso Ideal
+- 25 até 30: Sobrepeso
+- 30 até 40: Obesidade
+- Acima de 40: Obesidade Mórbida
+IMC = peso/altura**2
+'''
+
+while True:
+	try:
+		peso = float(input('Digite seu peso(Kg): '))
+	except:
+		print('Ocorreu um erro, digite novamente, não se esqueça de usar ponto no lugar da virgula')
+		continue
+	break		
+while True:
+	try:
+		altura = float(input('Digite sua altura(metros): '))
+	except:
+		print('Ocorreu um erro, digite novamente, não se esqueça de usar ponto no lugar da virgula')
+		continue
+	break
+		
+IMC = peso/(altura**2)
+print(IMC)
+if IMC <18.5:
+	print('Está abaixo do peso')
+elif IMC < 25:
+	print('Você está com o peso ideal')
+elif IMC <30:
+	print('Você está com sobrepeso')
+elif IMC < 40:
+	print('Você está com obesidade')
+else:
+	print('Você está com obesidade Mórbida')
