d03ed5c9bd19d783dd5402d46844d8e4d1cf8d11
Paulo Henrique Ito Gabriel <40235350+PauloHIG@users.noreply.github.com>
2021-02-02 11:58:51 -0300

    Add files via upload

---
+++ pythonCEV/Exercicios/ex77Vogais.py
@@ -0,0 +1,13 @@
+'''Exercício Python 077: Crie um programa que tenha uma tupla com várias palavras (não usar acentos). 
+Depois disso, você deve mostrar, para cada palavra, quais são as suas vogais.'''
+palavras = 'Paulo','ABACATE','PEIXE','TUPLA','ALFABETO','PYTHON'
+
+for palavra in palavras:
+	print(f'A palavra {palavra} tem as vogais:',end=' ')
+	for letra in palavra:
+		print(letra if letra.lower() in 'aeiou'else '',end='')
+	print('')
+
+print('\nPalavras ao contrario só de brincadeira\n')
+for palavra in palavras:
+	print(palavra[::-1])
