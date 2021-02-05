c198bcff6abc5a1a369b9ec0a8a7e5681f6cb0d7
Paulo Henrique Ito Gabriel <40235350+PauloHIG@users.noreply.github.com>
2021-02-05 00:20:27 -0300

    Add files via upload

--- pythonCEV/Exercicios/ex87Matriz2.py
+++ pythonCEV/Exercicios/ex87Matriz2.py
@@ -12,24 +12,24 @@ def Recint(i,j):
 			continue
 		break
 	return n
-
+#inserindo dados na matriz
 for i in range(1,4):
 	for j in range(1,4):
 		matriz[i-1][j-1] = Recint(i,j)
-
+#exibindo a matriz
 for linha in matriz:
 	for coluna in linha:
 		print(f'|{coluna:5}|',end='')
 	print('')
-
+#somando os pares
 soma = 0
 for ele in matriz:
 	for num in ele:
 		if num % 2 == 0:
 			soma += num
 print(f'Soma de todos os valores pares digitados = {soma}')
-soma=0
+soma=0#estou reutilizando essa variável
 for i in range(3):
-	soma += matriz[i][2]
+	soma += matriz[i][2]#a coluna está presa no 2, equivalente a terceira culuna pedida no ex
 print(f'Soma de todos os valores da terceira coluna = {soma}')
 print(f'Maior valor da segunda linha = {max(matriz[1])}')
