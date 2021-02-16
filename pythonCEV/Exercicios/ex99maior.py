2c4beaef00b42374b31d51ee47759e5ff757c7d1
Paulo Henrique Ito Gabriel <40235350+PauloHIG@users.noreply.github.com>
2021-02-15 22:54:47 -0300

    Add files via upload

---
+++ pythonCEV/Exercicios/ex99maior.py
@@ -0,0 +1,15 @@
+'''Exercício Python 099: Faça um programa que tenha uma função chamada maior(), que receba vários parâmetros com valores inteiros. Seu 
+programa tem que analisar todos os valores e dizer qual deles é o maior.'''
+def maior(* numeros):#com o asterisco, posso colocar um número indefinido de variáveis,e, nesse caso especifico, usar elas como uma lista
+	maior=cont=0
+	for n in numeros:
+		if cont == 0:#é absolutamente necessário fazer isso, caso o maior número seja menor que 0
+			maior = n
+		if maior < n:
+			maior = n
+		cont+=1
+	return maior
+
+	
+print(maior(-1,-2,-4))
+print(maior(6,3,-2,-100))
