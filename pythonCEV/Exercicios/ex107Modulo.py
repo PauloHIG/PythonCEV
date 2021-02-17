daa0eab59b1485cb49b9e894cb09faf769d9133f
Paulo Henrique Ito Gabriel <40235350+PauloHIG@users.noreply.github.com>
2021-02-17 19:32:26 -0300

    Add files via upload

---
+++ pythonCEV/Exercicios/ex107Modulo.py
@@ -0,0 +1,7 @@
+import ex107_pacote as pc#importado da pasta(ou seja, do pacote) e pegando as funções que estão dentro do arquivo __init__
+print(pc.dobro(20))
+print(pc.metade(20))
+print(pc.aumentar(100,13))
+print(pc.diminuir(100,10))
+'''Exercício Python 107: Crie um módulo chamado moeda.py que tenha as funções incorporadas aumentar(), diminuir(), dobro() e metade().
+Faça também um programa que importe esse módulo e use algumas dessas funções.'''
---
+++ pythonCEV/Exercicios/ex107_pacote/__init__.py
@@ -0,0 +1,9 @@
+#é aqui dentro do init que coloco as funções que usarei em outro programa
+def metade(valor):
+	return valor/2
+def dobro(valor):
+	return valor*2
+def aumentar(valor,porcentagem):
+	return(valor+(valor*(porcentagem/100)))
+def diminuir(valor,porcentagem):
+	return(valor-(valor*(porcentagem/100)))
