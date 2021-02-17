e5276517295f42ba261300dd56a022acabae831d
Paulo Henrique Ito Gabriel <40235350+PauloHIG@users.noreply.github.com>
2021-02-16 21:04:22 -0300

    Add files via upload

---
+++ pythonCEV/Exercicios/ex106menu.py
@@ -0,0 +1,15 @@
+'''Exercício Python 106: Faça um mini-sistema que utilize o Interactive Help do Python. O usuário vai digitar o comando e o manual vai 
+aparecer. Quando o usuário digitar a palavra 'FIM', o programa se encerrará. Importante: use cores.'''
+#usarei alguns modulos de cores já que o código de cores normal não está funcionando no meu sistema operacional
+from colorama import init
+from termcolor import colored
+init()
+# print(colored('batata','red','on_white'))
+a=''
+print(colored('Manual de funções e bibliotecas do Python',None,'on_green'))
+while True:
+	a = input('Quer ajuda em qual função ou biblioteca?: ')
+	if a.upper()=='FIM':
+		break
+	colored('','blue','on_white')
+	help(a)
