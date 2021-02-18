3759516693986a6f2f273a0b06686a8cda352d05
Paulo Henrique Ito Gabriel <40235350+PauloHIG@users.noreply.github.com>
2021-02-18 17:20:16 -0300

    Add files via upload

--- pythonCEV/Exercicios/ex107_pacote/__init__.py
+++ pythonCEV/Exercicios/ex107_pacote/__init__.py
@@ -1,9 +1,11 @@
 #é aqui dentro do init que coloco as funções que usarei em outro programa
-def metade(valor):
+def metade(valor=0):
 	return valor/2
-def dobro(valor):
+def dobro(valor=0):
 	return valor*2
-def aumentar(valor,porcentagem):
+def aumentar(valor=0,porcentagem=0):
 	return(valor+(valor*(porcentagem/100)))
-def diminuir(valor,porcentagem):
+def diminuir(valor=0,porcentagem=0):
 	return(valor-(valor*(porcentagem/100)))
+def moeda(n=0):
+	print(f'R$ {n:.2f}'.replace('.',','))
---
+++ pythonCEV/Exercicios/ex108Moeda.py
@@ -0,0 +1,4 @@
+#ex108Moeda
+from ex107_pacote import moeda
+
+moeda(12.99)
