8435e1203c9504490744836fd03d6d57e2e88773
Paulo H. Ito <pauloito123@gmail.com>
2023-04-21 21:36:22 -0300

    reduzir linhas de código é sempre bom

--- pythonCEV/Exercicios/ex03Calculadora.py
+++ pythonCEV/Exercicios/ex03Calculadora.py
@@ -2,18 +2,14 @@
 n1 = int(input("Digite o primeiro numero "))
 n2 = int(input("Digite o segundo numero "))
 
-
 def soma(n1, n2):
     return n1 + n2
-
 
 def multi(n1, n2):
     return n1 * n2
 
-
 def divint(n1, n2):
     return int(n1 / n2)
-
 
 def div(n1, n2):
     return n1 / n2
@@ -31,7 +27,7 @@ def pot(n1, n2):
     return n1 ** n2
 
 
-print("A soma é: {}".format(soma(n1, n2)))
-print("A subtração é:{}".format(subtracao(n1, n2)))
-print("O resultado da divisão inteira de {}/{} é {} e o resto é {}".format(n1, n2, divint(n1, n2), restoDivi(n1, n2)))
-print("{} elevado a {} é {}".format(n1, n2, pot(n1, n2)))
+print(f"A soma é: {soma(n1, n2)}")
+print(f"A subtração é:{subtracao(n1, n2)}")
+print(f"O resultado da divisão inteira de {n1}/{n2} é {divint(n1, n2)} e o resto é {restoDivi(n1, n2)}")
+print(f"{n1} elevado a {n2} é {n1**n2}")
